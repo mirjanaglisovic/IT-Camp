af694ce0838a940bb4ced704849818fc6027ccad
mirjanaglisovic <mirjana.glisovic123@gmail.com>
2023-11-29 19:52:50 +0100

    1, 2, 3. cas

---
+++ Java Script/1. i 2. cas/app.js
@@ -0,0 +1,77 @@
+naslov = document.getElementById("naslov");
+naslov.innerHTML = "Naslov ispisan preko JavaScript";
+naslov.style = "color: blue; font-size: 45px";
+
+//TIPOVI PODATAKA //
+
+// Da bismo znali tip promenljive u Java Scriptu koristimo typeof operator.
+
+// Tipove podataka nu JavaScriptu se dele u dve grupe:
+
+// 1. Primitivni tipovi podataka (vrednosni)
+
+// 2. Neprimitivni ili referentni tipovi podataka
+
+// 1.1. Stringovi - Niz karaktera koji je zapisan pod navodnicima. Obicnim ili duplim.
+console.log("ovo je nas prvi string.");
+console.log(typeof "ovo je nas prvi string.");
+
+// 1.2. Number - Predstavlja broj i on moze biti zapisan sa ili bez decimalnog zareza.
+console.log(25);
+console.log(typeof 25);
+
+// 1.3. BigInt - Brojevi koji prelaze granice obicnih brojeva.
+console.log(BigInt(12345678987654321234567234567890098765432234567876543289));
+console.log(typeof BigInt(12345678987654321234567890098765432234567876543289));
+
+// 1.4. Boolean - Logicki etitet koji ima dve vrednosti true i false.
+console.log(true);
+console.log(typeof true);
+
+// 1.5. Undefined - Promenljiva koja je definisanaa ali joj nije dodeljena vrednost ima vrednost i tip takosje underfined.
+var a;
+console.log(a);
+console.log(typeof a);
+
+// 1.6. Null - Predstavlja nekorektnu vrednost ili vrednost koja (za sada) ne postoji.
+var a = null;
+console.log(a);
+console.log(typeof a);
+
+// 1.7. Symbol - Tip podataka za skladistenje anonimnih i unikatnioh vrednosti.
+b = Symbol("Demir");
+console.log(b);
+console.log(typeof b);
+
+// Primitivni tipovi podataka mogu skladistiti jednu vrednost
+
+// 2. Neprimitivni ili referentni tipovi podataka
+
+// Za skladistenje vise vrednosti ili komplesnih vrednosti se koriste ne primitivni tipovi podataka.
+// Svaka promenljiva koja nije primitivnog tipa je tipa Object u JavaScript-u.
+// Objekti se koriste za skladistenje neke lekcije podataka.
+
+//2.1. Array (Niz) - Predstavlja stukturu podataka za skadistenje vise vrednosti.
+// Elementi niza ne moraju biti istog tipa.
+
+nasBrat = ["Demir", 18, true];
+
+// 2.2. Object (Objekat) - Predstavlja strukturu podataka za skladistenje vise vrednosti.
+// Objekti se zapisuju u key:value (name:value) parovima.
+
+obj = { ime: "Demir", brojGodina: "18", punoletan: true };
+console.log(obj);
+
+// 2.3. Set = Predstavlja strukturu podataka za skladistenje vise vrednosti. Duplikati nisu doyvoljeni.
+
+set = new Set(["Kanita", 19, true]); // Druga Kanita se ne vidi
+console.log(Set);
+console.log(typeof Set);
+
+// 2.4. Map - Predstavlje strukturu za sklaistenje vise vrednosti. Slicni su objektima.
+
+map = new Map([
+  ["ime", "Kanita"],
+  ["brojGodina", 19],
+  ["punoletnost", true],
+]);
---
+++ Java Script/3.cas/app.js
@@ -0,0 +1,63 @@
+// Postoji nekoliko nacina za prikazivanje podataka u JavaScript-u:
+
+// 1. innerHTML:
+
+paragraf = document.getElementById("paragraf");
+paragraf.innerHTML = "Ovo je paragraf";
+
+// 2. document.write() - koristi se samo za testiranje.
+
+// 3. alert():
+
+//alert("Upozorenje Korisniku.");
+
+// 4. console.log()
+console.log(paragraf.innerHTML);
+
+// JavaScript name mora da pocinje sa:
+// 1. Velikim ili malim slovom (A-Z ili a-z),
+// 2. $ (dolar znakom),
+// 3. _ (donjom crtom).
+
+// Napomena: U nazivu promenljive ili varijable se moze naci broj, ali ona se ne sme poceti brojem.
+
+amir = "Amir je odlican ucenik prve godine srednje skole.";
+Amir = "Amir je dovoljan ucenik prve godine srednje skole.";
+
+// JavaScript je case sencitive!!! (Razlikuje velika i mala slova)
+
+console.log(amir);
+console.log(Amir);
+
+// Postoje 4 nacina da deklarisemo promenljivu u JavaScript-u:
+
+// 1. Koriscenje var heyword (rezervisane reci) - prvenstveno koriscen na ranijim browserima;
+
+// var prijatno = "Prijatno!"; primer istovremenog deklarisanje i inicijalizovanje promenljive.
+
+var prijatno; // deklarisanje promenljive
+console.log(prijatno); // undefined
+console.log(typeof prijatno); // undefined
+
+projatno = "Prijatno!"; //inicijalizacija deklarisane promenljive prijatno
+
+// 2. Koriscenje let kezword (rezervisane reci) - koristimo kada je vrednost promenljive ili varijable sklono menjanju.
+
+//let brojGodina = 25 primer istovremenog deklarisanje i inicijalizovanje promenljive pomocu let.
+
+let brojGodina; // deklarisanje promenljive
+brojGodina = 27; // inicijalizacija deklarisane promenljive brojGodina
+
+//3. Koriscenje let kezword (rezervisane reci) - koristimo za vrednosti koje se ne menjaju.
+
+//const ime = "Dzenan"; primer istovremenog deklaracije i inicijacije promenljive pomocu const
+
+// koriscenjeconst keyword nije dozvoljeno posebno inicijalizovanje, vec mora uz deklaraciju zajedno.
+// const ime;
+// ime = "Dzenan"
+
+// 4. koriscenje niceg.
+
+mervan = "Mervan";
+
+// Napomeena promenljive ili varijable su kontejneri za skladistenje vrednosti.
