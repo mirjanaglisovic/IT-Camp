32d1353b507b3a36376c4cb0ede396bc850650e5
mirjanaglisovic <mirjana.glisovic123@gmail.com>
2023-12-06 12:43:38 +0100

    3.cas

---
+++ Java Script/4 i 5.cas/app.js
@@ -0,0 +1,43 @@
+// Promnljive definisane preko var kezword-a mozemo ponovo deklarisati (izvrsiti redeklaraciju), kao i renicijalizaciju (novo dodeljivanje vrednosti).
+
+var x = 10;
+console.log(x);
+
+x = 14;
+console.log(x);
+
+var x = 15;
+console.log(x);
+
+// Let i const keywords nam obezbedjuju Block Scope.
+
+// Promnljive definisane preko let kezword-a mozemo ponovo deklarisati (izvrsiti redeklaraciju) ako se nalazimo u drugom prostoru, dok renicijalizaciju (novo dodeljivanje vrednosti) mozemo izvrsiti u istom prostoru.
+
+let y = 3;
+console.log(y);
+
+y = 6;
+console.log(y);
+// let y = 50; nije dozvoljeno redeklarisanje u istom prostoru.
+
+{
+  // console.log(y); y se prepoznaje iz global scope.
+  let y = 20;
+  console.log(y);
+}
+console.log(y);
+
+// Promnljive definisane preko const kezword-a mozemo ponovo deklarisati (izvrsiti redeklaraciju), kao i renicijalizaciju (novo dodeljivanje vrednosti) ako se radi o drugom prostoru.
+// Reinicijalizacija nije dozvoljena
+
+const z = 30;
+
+// const z = 45; nije dozvoljeno redeklarisanje u istom prostoru.
+// z = 60; nije dozvoljena reinicilalizacija u istom prostoru.
+
+{
+  //console.log(z); z se prepoznaje iz global scope.
+  let z = 90;
+  console.log(z);
+}
+console.log(z);
---
+++ Java Script/6.cas/app.js
@@ -0,0 +1,40 @@
+// KONDICIONALI //
+
+// (5 > 4) je uslov koji kada je zadovoljen izvrsice se deo koda izmedju viticastih zagrada.
+
+if (5 > 4) {
+  console.log("vece je");
+}
+
+trenutnoVreme = 20;
+
+if (trenutnoVreme >= 6 && trenutnoVreme < 12) {
+  console.log("Dobro jutro");
+} else if (trenutnoVreme >= 12 && trenutnoVreme < 17) {
+  console.log("Dobar dan");
+} else {
+  console.log("Dobro vece");
+}
+
+brojGodina = 21;
+
+if (brojGodina >= 7 % brojGodina < 13) {
+  console.log("Punoletni ste");
+} else {
+  console.log("Maloletni ste");
+}
+
+if (brojGodina >= 18) {
+  console.log("Punoletni ste");
+} else {
+  console.log("Maloletni ste");
+}
+
+const x = 12;
+const y = 6;
+
+if (y === 0) {
+  console.log("Deljenje je nemoguce");
+} else {
+  console.log(x / y);
+}
