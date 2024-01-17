d8cb70ca42be5a8abc9c0ecba1097655d5b925ec
mirjanaglisovic <mirjana.glisovic123@gmail.com>
2024-01-17 19:28:34 +0100

    23.cas

---
+++ Java Script/23.cas/domaci.js
@@ -0,0 +1,19 @@
+// Domaci zadaci:
+
+// 1.	Iz unetog stringa treba izdvojiti N znakova sa leve strane. Npr. za string 'Pera ima devojku' i N=6, dobija se 'Pera i'. Ako je N veće od dužine stringa, kao rezultat se dobija ceo string.
+
+function znakoviSaLeveStrane(str, N) {
+  if (N >= str.length) {
+    return str;
+  } else {
+    return str.slice(0, N);
+  }
+}
+
+let originalniString = "Pera ima devojku";
+let N = 6;
+let rezultat = znakoviSaLeveStrane(originalniString, N);
+
+console.log(rezultat);
+
+// 2. Izdvojiti N znakova sa desne strane (poslednjih N znakova) unetog stringa. Npr. za string 'Pera ima devojku' i N=5, dobija se 'vojku'.
