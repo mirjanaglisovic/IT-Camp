dc6be6afc4a383528a9817e1600cfca491562e62
mirjanaglisovic <mirjana.glisovic123@gmail.com>
2024-01-29 21:11:43 +0100

    29.cas

---
+++ Java Script/29.cas/app.js
@@ -0,0 +1,43 @@
+// primitives
+// complex types
+// const let
+// operators
+// logical operators
+// conditionals
+// loops ✅
+// objects
+// arrays
+// array methods
+// callbacks
+
+// promises
+// event loop
+// async await
+// DOM & BOM
+// DOM manipulation
+// events
+
+// hoisting
+// closure
+// classes
+// es6 js
+
+// var a = "erf";
+
+// const a = 20;
+
+// let b = 30;
+
+console.log(1 + "2" + "false" + false); // 12falsefalse
+console.log(1 + 2 + true + false); // 4
+
+// truthy vs falsy
+// Za falsy: 0, NaN, undefined, null, false
+
+if (!!!!!!document) {
+  console.log("radi");
+}
+
+console.log("" || false || "true");
+console.log("" || 0 || false);
+console.log("yes" || 0 || false);
