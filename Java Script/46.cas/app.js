10fd25fd0190dc14cc66bfa951b458e6d62bf2b3
mirjanaglisovic <mirjana.glisovic123@gmail.com>
2024-03-22 12:16:41 +0100

    46.cas

--- Java Script/46.cas/app.js
+++ Java Script/46.cas/app.js
@@ -9,16 +9,51 @@
 // najvise pitanja iz simple js i 2-3 pitanja iz advanced i medium js
 
 // select a primitive js type? i onda abcd, odgovori
+// String, Number, BigInt, Boolean, Null, Undefined, and Symbol.
+
 // what are complext types in js? abcd
+// represents a number of similar or different data under a single declaration of a variable.
+
 // what are the scopes of const, let, and var
+// let and const are block-scoped, var declarations are either globally scoped or function-scoped.
+
 // what are the differces between let and const...
+
+// Use let if you want block-scoped variables that can be reassigned.
+// Use const if you want block-scoped variables that are constant and cannot be reassigned.
+
 // what is the result of the code console.log("yes" || "no")?
+
+console.log("yes" || "no");
+
 // what is the result of the code if(true){console.log(true)}
+
+const recenica = 0;
+
+for (let i = 0; i < recenica.length; i++) {
+  if (true) {
+    console.log(true);
+  }
+}
+
 // what are the loops use in js?
+// Loops are used to execute a set of instructions repeatedly until a particular condition is met.
+
 // what is an object in js?
+
 // how do we dinamically access values inside of the object?
 // What are the property accessor for objects?
+// kad znamo ime objekta . i dinamicko []
+
 // Which are the array fuctions?
+
+// There are different types of array methods based on their functionality:
+// Manipulation Methods: push(), pop(),shift(), slice(), etc.
+// Searching and Sorting Methods: sort(), find(),indexof(), etc.
+// Conversion Methods: join(), concat(),tostring().
+// Iteration Methods: foreach(), map(), filter(), etc.
+// Utility Methods: isArray()
+
 // What does this code do? [].map(()=>{})
 // advanced only definitions of concepts.
 
@@ -40,5 +75,3 @@
 // Moguca dodatna komplikacija gde ce te morati i na neki event da fetchate podatke
 
 // imate primere od 9 casa pa nadalje. A navise na casu 15.
-
-// Q&A
