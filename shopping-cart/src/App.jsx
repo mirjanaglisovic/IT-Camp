ca7ca505698da3e186d76b1679e567ef396ba7ab
mirjanaglisovic <mirjana.glisovic123@gmail.com>
2024-07-12 14:36:26 +0200

    header, footer

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,7 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-
+import Header from "./commponents/Header/Header";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Footer from "./commponents/Footer/Footer";
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Header />
+      <main className=""></main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ shopping-cart/src/commponents/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <h1 style={{ fontSize: "30px", marginLeft: "50px" }}>ShoppingCart ©</h1>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
